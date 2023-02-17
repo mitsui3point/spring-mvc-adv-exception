659dac4396e8fad0672862123c8c2594ce1199e5
mitsui3point <rladbsgh0331@gmail.com>
2023-02-17 20:01:46 +0900

    refactor($exception, $test.exception, $resolver, $test.resolver): move resolver class exception package to resolver package

--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -1,6 +1,7 @@
-package hello.exception.exception;
+package hello.exception.resolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.exception.exception.UserException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
